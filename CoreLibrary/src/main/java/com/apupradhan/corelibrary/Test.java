8548602b5f63c875163a2ec375cdfb71c7e36b13
godfindapu@gmail.com <Universe2020#>
2022-03-05 23:34:25 +0530

    Test

--- CoreLibrary/src/main/java/com/apupradhan/corelibrary/Test.java
+++ CoreLibrary/src/main/java/com/apupradhan/corelibrary/Test.java
@@ -6,7 +6,7 @@ import android.widget.Toast;
 public class Test {
 
     public static void NamasteWorld(Context ctx) {
-        Toast.makeText(ctx, "Namaste world...", Toast.LENGTH_SHORT).show();
+        Toast.makeText(ctx, "Namaste India...", Toast.LENGTH_SHORT).show();
     }
 
 }
