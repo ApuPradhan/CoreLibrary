6e110f76538b34464358792b6128561cb7f7df6f
godfindapu@gmail.com <Universe2020#>
2022-03-05 21:45:52 +0530

    Initial commit

---
+++ CoreLibrary/src/main/java/com/apupradhan/corelibrary/Test.java
@@ -0,0 +1,12 @@
+package com.apupradhan.corelibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Test {
+
+    public static void NamasteWorld(Context ctx) {
+        Toast.makeText(ctx, "Namaste world...", Toast.LENGTH_SHORT).show();
+    }
+
+}
